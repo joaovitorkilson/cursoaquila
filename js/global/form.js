db8fd9ea73c7f5b83ed0d009578e0c1bcfd1fdfa
Joao vitor <joaovitorkilson@gmail.com>
2022-10-28 15:27:01 -0300

    Formulario javascript

---
+++ js/global/form.js
@@ -0,0 +1,31 @@
+export default function formularioJS() {
+
+    const formulario = document.querySelector('#form')
+
+    function formularioEnviado(resposta) {
+      if (resposta.ok) {
+        formulario.innerHTML = "<p class='body-1' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #CCCCCC'><span style='color=#317a00;'>Mensagem enviada, em breve entraremos em contato. Geralmente respondemos em 24 horas.</span></p>"
+      } else {
+        formulario.innerHTML = "<p class='body-1' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #CCCCCC'><span style='color: #E00000;'>Erro no envio</span>, você pode enviar diretamente para o nosso email em: contato@aquilacursobq.com.br</p>"
+      }
+    }
+
+
+    function enviarFormulario(event) {
+      event.preventDefault()
+      const botao = document.querySelector('#form button')
+      botao.disabled = true
+      botao.innerText = "Enviando..."
+
+      const data = new FormData(formulario)
+
+      fetch('./enviar.php', {
+        method: 'POST',
+        body: data
+      })
+      .then(formularioEnviado)
+    }
+
+    formulario.addEventListener('submit', enviarFormulario)
+    
+}
